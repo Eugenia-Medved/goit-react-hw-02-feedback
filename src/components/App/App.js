5e8b8889854a539693738def8bcf51c563fed724
YevheniiaBondarenko <65921369+YevheniiaBondarenko@users.noreply.github.com>
2021-01-20 20:00:55 +0200

    finish

---
+++ src/components/App/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './App.css';
+import Container from 'components/Container/Container';
+import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
+import Statistics from 'components/Statistics/Statistics';
+import Notification from 'components/Notification/Notification';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = option => {
+    this.setState({ [option]: this.state[option] + 1 });
+  };
+
+  render() {
+    const total = this.state.good + this.state.neutral + this.state.bad;
+    const positivePercentage = Math.round((this.state.good / total) * 100);
+
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <Container title="Please leave feadback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Container>
+
+        <Container title="Statistics">
+          {total ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          ) : (
+            <Notification message="No feedback given!" />
+          )}
+        </Container>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import s from './FeedbackOptions.module.css';
+
+function FeedbackOptions({ options, onLeaveFeedback }) {
+  return (
+    <div className={s.container}>
+      {options.map(option => (
+        <button
+          type="button"
+          className={s.button}
+          key={option}
+          onClick={() => onLeaveFeedback(option)}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+FeedbackOptions.propTypes = {
+  onLeaveFeedback: PropTypes.func,
+  options: PropTypes.arrayOf(
+    PropTypes.exact({
+      option: PropTypes.object,
+    }),
+  ),
+};
+
+export default FeedbackOptions;
